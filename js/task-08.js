ee1391fc5a0a2120d51a8aae1142307827e711c6
JuliaZaykaS <sys55@mail.ru>
2021-05-14 11:36:47 +0300

    задача 8

--- js/task-08.js
+++ js/task-08.js
@@ -14,3 +14,55 @@
 // Каждый следующий div после первого,
 // должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+
+const inputEl = document.querySelector('[type="number"]');
+const createBtnEl = document.querySelector('[data-action="render"]');
+const cleanBtnEl = document.querySelector('[data-action="destroy"]');
+const divContainerEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', createBoxes);
+
+
+function getRandom(min, max){
+    return Math.ceil(Math.random() * (max - min) + min);
+}
+
+
+function createBoxes(amount) {
+    inputEl.addEventListener('input', OnChangeInputValue);
+
+    function OnChangeInputValue() {
+    amount = inputEl.value;
+    }
+    const arrayBoxes = [];
+
+    let size = 30;
+    for (let i = 0; i < inputEl.value; i += 1){
+            arrayBoxes[i] = document.createElement('div');
+            arrayBoxes[i].style.width = `${size}px`;
+            arrayBoxes[i].style.height = `${size}px`;
+            arrayBoxes[i].style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
+            arrayBoxes[i].classList.add('box');
+        size += 10;
+    };
+
+
+    return divContainerEl.append(...arrayBoxes);
+
+};
+
+
+cleanBtnEl.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+    const divChildEl = document.querySelectorAll('.box');
+        divChildEl.forEach((divChild) => {
+        divContainerEl.removeChild(divChild);
+        })
+
+    inputEl.value = '';
+
+}
+
+
+
+
